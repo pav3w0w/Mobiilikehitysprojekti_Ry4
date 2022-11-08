ccc0544433738e5483183c656b0b5bb122c8074f
Immu Saarijärvi <immu.saarijarvi@gmail.com>
2022-11-08 21:24:07 +0200

    first version of upvote/downvote

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import VoteButtons from './components/VoteButtons';
+import Constants from 'expo-constants';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <VoteButtons />
     </View>
   );
 }
@@ -14,7 +14,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    //alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? Constants.statusBarHeight : 0,
   },
 });
