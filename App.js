c696a979c50fe4770fcae2aad7a16f95669effcc
Foxglade <kapu.kettu@gmail.com>
2022-11-19 20:58:23 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Comments from './components/Comments'
 import { View, Text, Button, Image } from 'react-native';
 import Thread from './screens/Thread'
@@ -8,7 +9,33 @@ import NewThread from './screens/NewThread';
 import React, { useRef } from 'react';
 import ModalDropdown from 'react-native-modal-dropdown';
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
+
+
+function MyDrawer() {
+  return (
+  <NavigationContainer>
+    <Drawer.Navigator
+    screenOptions={{
+    headerShown:"true",
+    swipeEnabled:"false"
+    }}
+    >
+      <Drawer.Screen
+      name="Home"
+      component={Home}
+      />
+
+      <Drawer.Screen
+      name="NewThread"
+      component={NewThread}
+      />
+
+    </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
 
 
 export default function App() {
