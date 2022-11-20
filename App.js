4106e4b645cd463ddae534ea13fca6873a723fe5
Foxglade <kapu.kettu@gmail.com>
2022-11-20 14:40:03 +0200

    Drawer menu
    toimiva menu, jouduin jättämään stack.navigationin pois

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DrawerActions } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
 import Comments from './components/Comments'
 import { View, Text, Button, Image } from 'react-native';
 import Thread from './screens/Thread'
@@ -9,67 +14,69 @@ import NewThread from './screens/NewThread';
 import React, { useRef } from 'react';
 import ModalDropdown from 'react-native-modal-dropdown';
 const Stack = createNativeStackNavigator();
-const Drawer = createDrawerNavigator();
 
 
 
-function MyDrawer() {
+function Feed({ navigation }) {
   return (
-  <NavigationContainer>
-    <Drawer.Navigator
-    screenOptions={{
-    headerShown:"true",
-    swipeEnabled:"false"
-    }}
-    >
-      <Drawer.Screen
-      name="Home"
-      component={Home}
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Feed Screen</Text>
+      <Button
+        title="Thread"
+        onPress={() => navigation.navigate('Thread')}
       />
-
-      <Drawer.Screen
-      name="NewThread"
-      component={NewThread}
+      <Button
+        title="NewThread"
+        onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
       />
-
-    </Drawer.Navigator>
-    </NavigationContainer>
+    </View>
   );
 }
 
+function Notifications() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Notifications Screen</Text>
+    </View>
+  );
+}
+
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem
+        label="Sulje valikko"
+        onPress={() => props.navigation.dispatch(DrawerActions.closeDrawer())}
+      />
+
+    </DrawerContentScrollView>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator
+      useLegacyImplementation
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+    >
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Thread" component={Thread} />
+      <Drawer.Screen name="NewThread" component={NewThread} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
 
   return (
 
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen
-          name='Home'
-          component={Home}
-          options={{
-            headerRight: () => (
-
-              <ModalDropdown options={['option 1', 'option 2']} />
-
-            ),
-          }}
-
-        />
-        <Stack.Screen
-          name='Thread'
-          component={() => Thread("YIkO1keG7yIrFMcvWiUQ")}
-        />
-        <Stack.Screen
-          name='NewThread'
-          component={NewThread}
-          options={{
-            title: 'NewThread',
-            headerTitle: 'Create a new Thread',
-          }}
-        />
-      </Stack.Navigator>
+        <MyDrawer />
     </NavigationContainer>
+
 
   );
 }
