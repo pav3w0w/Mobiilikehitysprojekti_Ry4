ac0cdcad1a6a6b62268c0f026e387898cfaf4d4d
Jaakko Mursu <jaakko.mursu@f-solutions.fi>
2022-11-24 21:50:01 +0200

    Better styling and content compression

--- components/ThreadPreview.js
+++ components/ThreadPreview.js
@@ -1,27 +1,32 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
-export default function ThreadPreview(props, navigation) {
 
+export default function ThreadPreview(props, navProp) {
 
     const shorten = (content) => {
         if (content.length > 50) {
-            return content.slice(0, 47) + "..."
+            return content.slice(0, 40) + "..."
+
         }
         return content
     }
 
     return (
-        <View onStartShouldSetResponder={() => navigation.navigate("Thread", props.threadId)}>
+        <View style={styles.ThreadPreview} onStartShouldSetResponder={() => navProp.navigate('Thread', { screen: 'Thread', threadId: props.threadId })}>
             <View style={styles.titleContainer}>
                 <Text style={styles.title}>{props.title}</Text>
             </View>
-            <Text style={styles.OPtext}>{() => shorten(props.content)}</Text>
+            <Text style={styles.OPtext}>{shorten(props.content)}</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    ThreadPreview: {
+        paddingTop: 20,
+        paddingBottom: 20
+    },
     titleContainer:
     {
         flexDirection: 'row',
@@ -33,7 +38,7 @@ const styles = StyleSheet.create({
     },
     OPtext: {
         fontSize: 18,
-        marginTop: 20,
+        marginTop: 10,
         marginLeft: 20,
         marginRight: 20
     },
