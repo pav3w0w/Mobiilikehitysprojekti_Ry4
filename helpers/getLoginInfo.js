803a0600c3859c426fca06c844f21f80c524b67d
Jaakko Mursu <jaakko.mursu@f-solutions.fi>
2022-12-15 23:31:04 +0200

    Login now stores user data using expo-secure-store
    This makes handling userId's easier in other screens securely

---
+++ helpers/getLoginInfo.js
@@ -0,0 +1,10 @@
+import * as SecureStore from 'expo-secure-store'
+
+export async function getLogin() {
+    return { "id": await SecureStore.getItemAsync("userId"), "jwt": await SecureStore.getItemAsync("jwt") }
+}
+
+export async function saveLogin(jwt, userid) {
+    await SecureStore.setItemAsync("jwt", jwt)
+    await SecureStore.setItemAsync("userId", userid)
+}
--- screens/Login.js
+++ screens/Login.js
@@ -1,18 +1,21 @@
 import { View, Text, Button, TextInput, StyleSheet } from 'react-native'
 import React, { useState } from 'react'
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
-
+import { saveLogin } from '../helpers/getLoginInfo'
 
 export default function LoginScreen({ route, navigation }) {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const auth = getAuth();
 
+
+
     const tryLogin = () => {
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredentials) => {
-                navigation.navigate('Home', { ...route, "params": { "userData": userCredentials._tokenResponse } })
-            })
+                console.log(userCredentials._tokenResponse)
+                saveLogin(userCredentials._tokenResponse.idToken, userCredentials._tokenResponse.localId)
+            }).then(navigation.navigate('Home'))
             .catch((error) => {
                 console.error(error);
             })
@@ -33,6 +36,8 @@ export default function LoginScreen({ route, navigation }) {
                 secureTextEntry={true}
             />
             <Button onPress={() => tryLogin()} title="Login"></Button>
+            <Text>Or register here</Text>
+            <Button onPress={() => navigation.navigate('Register')} title="register" />
         </View>
     )
 }
