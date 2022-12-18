6f07863003cc01b76e678d85c602a1de316f35a7
Jaakko Mursu <jaakko.mursu@f-solutions.fi>
2022-12-18 20:09:33 +0200

    Tell unregistered user to sign in when necessary

--- helpers/votes.js
+++ helpers/votes.js
@@ -1,29 +1,31 @@
 import { doc, getFirestore, updateDoc } from "firebase/firestore"
 import { db } from '../dbConn'
+import Toast from 'react-native-root-toast'
 
-const updateVotes = async (objectId, objectType, newVotes) => {
+
+const updateVotes = async (objectId, objectType, newVotes, voteSetter) => {
     const fireStore = getFirestore(db)
     await updateDoc(doc(fireStore, objectType, objectId), {
         upvotes: newVotes.upvotes,
         downvotes: newVotes.downvotes
     }).then(() => {
+        voteSetter({ upvotes: newVotes.upvotes, downvotes: newVotes.downvotes })
         console.log("data updated")
     }).catch((error) => {
-        console.log(error)
+        if (error.name == "FirebaseError") {
+            Toast.show("You need to be signed in to vote", { duration: Toast.durations.LONG })
+        }
     })
 }
 
 export function downvote(currentVotes, voteSetter, objectId, objectType) {
     var newVotes = currentVotes
     currentVotes.downvotes = currentVotes.downvotes - 1
-    voteSetter({ upvotes: newVotes.upvotes, downvotes: newVotes.downvotes })
-    updateVotes(objectId, objectType, newVotes)
+    updateVotes(objectId, objectType, newVotes, voteSetter)
 }
 
 export function upvote(currentVotes, voteSetter, objectId, objectType) {
     var newVotes = currentVotes
     newVotes.upvotes = newVotes.upvotes + 1
-    console.log(voteSetter)
-    voteSetter({ upvotes: newVotes.upvotes, downvotes: newVotes.downvotes })
-    updateVotes(objectId, objectType, newVotes)
+    updateVotes(objectId, objectType, newVotes, voteSetter)
 }
